fb2a8f1c07161018d6ba3cc56a0758155105d908
MatthewNoyce <131261051+MatthewNoyce@users.noreply.github.com>
2026-08-31 16:17:41 +0100

    Add files via upload

--- testing.py
+++ testing.py
@@ -1,11 +1,12 @@
 import ValidParenthesis as vp
+import PlusOne as po
 import ctypes 
 
 #Creates a little dialogue box (for fun)
 def Mbox(title, text, style):
     return ctypes.windll.user32.MessageBoxW(0, text, title, style)
 
-def testCases():
+def testCasesVP():
     output = True
     test = vp.Solution
     testList = ["()","()[]{}","(]", "([])", "([)]"]
@@ -19,8 +20,26 @@ def testCases():
             return False
     return output
 
+def testCasesPO():
+    output = True
+    testList = [[1,2,3], [4,3,2,1], [9], [9,9,9]]
+    solns = [[1,2,4], [4,3,2,2], [1,0], [1,0,0,0]]
+    test = po.Solution
+    for i in testList:
+        idx = testList.index(i)
+        if solns[idx] == test.plusOne(i):
+            output = True
+        else:
+            return False
+    return output
 
-if testCases():
-    Mbox('Passed', 'All Tests Passed', 0)
+if testCasesVP():
+    Mbox('Passed', 'All Tests Passed For Valid Parenthesis', 0)
 else:
     Mbox('Failed', 'One or More Tests Failed', 0)
+
+if testCasesVP():
+    Mbox('Passed', 'All Tests Passed For Plus One', 0)
+else:
+    Mbox('Failed', 'One or More Tests Failed', 0)
+
